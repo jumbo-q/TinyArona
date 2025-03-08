af844152549e07e76402f2c80827a45e988487e4
QI, Junbo <junboqi.2003@gmail.com>
2025-03-08 12:26:47 +0800

    Delete 1.py

--- 1.py
+++
@@ -1,5 +0,0 @@
-import tiktoken
-from src.config import ModelConfig
-enc = tiktoken.get_encoding(ModelConfig.encoding_type)
-print("你好呀",enc.encode("你好呀"))
-print("你好呀",enc.decode([19526, 254, 25001, 121, 37772, 222]))
